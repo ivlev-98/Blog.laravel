6f7e14f8a02d643935aa4863b7227f0209933148
Александр Ивлев <aivlev.98@yandex.ru>
2022-04-29 22:18:55 +0300

    Add column to posts. Migrations for comments

---
+++ database/factories/CommentsFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comments>
+ */
+class CommentsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'post_id' => rand(1, 80),
+            'user_id' => rand(1, 10),
+            'content' => $this->faker->realText(200)
+        ];
+    }
+}
